0be9b9ba48be4f12dcdfc9e519fc5037cd4e0937
gungho0619 <tzztntt@gmail.com>
2023-12-24 21:34:21 -0500

    chore: Update repo name in config to reflect changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-UI-Clone/"
+  base: "/Netflix-Clone/"
 });
