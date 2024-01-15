e7e92d849b3d92e78caa48c66074935f0789b2f9
gungho0619 <tzztntt@gmail.com>
2024-01-15 15:51:58 -0500

    docs: update banner to display WIP status

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <div className="w-full text-center py-1 bg-red-500 text-zinc-50">
-      This is a UI clone of Netflix.
+      Work In Progress. This is a UI clone of Netflix.
     </div>
   );
 }
